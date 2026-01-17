d7e5db85e6ce6899ca8097eb0d5e0e5107d43b61
Soham Sarkar <sohamsarkar552@gmail.com>
2026-01-17 12:28:52 +0000

    add variables notes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1561267
+let accountEmail = "soham@google.com"
+var accountPassword = "54672"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 8
+
+accountEmail = "sdgda@google.com"
+accountPassword = "5796436"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
