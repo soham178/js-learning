4a7b18460aa151b483f1271ee31741f1525a53ca
Soham Sarkar <sohamsarkar552@gmail.com>
2026-01-19 10:11:14 +0000

    data type summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //answer is false
+
+const bigNumber = 16468745314568764531256974513n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Soham",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
