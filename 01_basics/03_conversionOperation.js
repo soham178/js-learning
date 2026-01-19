9fb97e5d4ab3a8571fa19ef048c182c91d023f21
Soham Sarkar <sohamsarkar552@gmail.com>
2026-01-19 09:41:51 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,47 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// *************** Operations ******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+/* console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3); */
+
+let str1 = "hello"
+let str2 = " Soham"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);  //answer is 12
+console.log(1 + "2");  //answer is 12
+console.log("1" + 2 +2);  //answer is 122
+console.log(1 + 2 + "2");  //answer is 32
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
